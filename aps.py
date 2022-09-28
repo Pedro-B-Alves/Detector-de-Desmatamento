82731a4ded302590abbe6c3760fb047bc3e44d96
Pedro-B-Alves <pedro22carro@gmail.com>
2022-09-28 16:05:18 -0300

    atualizacao do caminho da imagem

--- aps.py
+++ aps.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
-img = mpimg.imread("C:\\Users\\pedro\\OneDrive\\Área de Trabalho\\6semestre\\APS\\Detector-de-arvores\\img\\arvore.jpg")
+img = mpimg.imread("img\\floresta.jpg")
 
 R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
 imgGray = 0.2989 * R + 0.5870 * G + 0.1140 * B
