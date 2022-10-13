67966fe56d9fd66abd4228456e781dbee0a6c490
Pedro-B-Alves <pedro22carro@gmail.com>
2022-10-12 22:14:43 -0300

    Detecção de bordas

--- aps.py
+++ aps.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
 
 img = mpimg.imread("img\\floresta.jpg")
 
@@ -7,3 +8,18 @@ R, G, B = img[:,:,0], img[:,:,1], img[:,:,2]
 imgGray = 0.2989 * R + 0.5870 * G + 0.1140 * B
 plt.imshow(imgGray, cmap='gray')
 plt.show()
+
+Gx = np.array([[1.0, 0.0, -1.0], [2.0, 0.0, -2.0], [1.0, 0.0, -1.0]])
+Gy = np.array([[1.0, 2.0, 1.0], [0.0, 0.0, 0.0], [-1.0, -2.0, -1.0]])
+[rows, columns] = np.shape(imgGray)
+sobel_filtered_image = np.zeros(shape=(rows, columns))
+
+for i in range(rows - 2):
+    for j in range(columns - 2):
+        gx = np.sum(np.multiply(Gx, imgGray[i:i + 3, j:j + 3]))  # direcao x
+        gy = np.sum(np.multiply(Gy, imgGray[i:i + 3, j:j + 3]))  # direcao y
+        sobel_filtered_image[i + 1, j + 1] = np.sqrt(gx ** 2 + gy ** 2)  # calcula a "hipotenusa"
+
+plt.imshow(sobel_filtered_image, cmap='gray')
+plt.show()
+
